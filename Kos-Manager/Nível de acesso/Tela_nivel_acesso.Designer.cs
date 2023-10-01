f62a5332132c32d892b892d6557b5e9af7d7b76d
SaulooDev <saulo.castrodev@gmail.com>
2023-09-30 21:13:39 -0300

    Coloquei os componentes da tela niveis de acesso

--- Kos-Manager/Nível de acesso/Tela_nivel_acesso.Designer.cs
+++ Kos-Manager/Nível de acesso/Tela_nivel_acesso.Designer.cs
@@ -28,20 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tela_nivel_acesso));
+            this.Lbl_estoque = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txt_buscar_nivel = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txt_buscar_nivel_id = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btn_adicionar = new Guna.UI2.WinForms.Guna2Button();
             this.SuspendLayout();
+            // 
+            // Lbl_estoque
+            // 
+            this.Lbl_estoque.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Lbl_estoque.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_estoque.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.Lbl_estoque.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(210)))), ((int)(((byte)(197)))));
+            this.Lbl_estoque.Location = new System.Drawing.Point(463, 59);
+            this.Lbl_estoque.Name = "Lbl_estoque";
+            this.Lbl_estoque.Size = new System.Drawing.Size(188, 32);
+            this.Lbl_estoque.TabIndex = 39;
+            this.Lbl_estoque.Text = "NÍVEIS DE ACESSO";
+            this.Lbl_estoque.TextAlignment = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(210)))), ((int)(((byte)(197)))));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(180, 120);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(119, 23);
+            this.guna2HtmlLabel2.TabIndex = 40;
+            this.guna2HtmlLabel2.Text = "Nível de Acesso";
+            // 
+            // txt_buscar_nivel
+            // 
+            this.txt_buscar_nivel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txt_buscar_nivel.BorderRadius = 10;
+            this.txt_buscar_nivel.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_buscar_nivel.DefaultText = "";
+            this.txt_buscar_nivel.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_buscar_nivel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_buscar_nivel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_buscar_nivel.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_buscar_nivel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txt_buscar_nivel.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_buscar_nivel.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.txt_buscar_nivel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.txt_buscar_nivel.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_buscar_nivel.Location = new System.Drawing.Point(180, 155);
+            this.txt_buscar_nivel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_buscar_nivel.Name = "txt_buscar_nivel";
+            this.txt_buscar_nivel.PasswordChar = '\0';
+            this.txt_buscar_nivel.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(151)))), ((int)(((byte)(151)))));
+            this.txt_buscar_nivel.PlaceholderText = "Buscar nível de acesso";
+            this.txt_buscar_nivel.SelectedText = "";
+            this.txt_buscar_nivel.Size = new System.Drawing.Size(285, 36);
+            this.txt_buscar_nivel.TabIndex = 41;
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(210)))), ((int)(((byte)(197)))));
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(556, 120);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(19, 23);
+            this.guna2HtmlLabel3.TabIndex = 42;
+            this.guna2HtmlLabel3.Text = "ID";
+            // 
+            // txt_buscar_nivel_id
+            // 
+            this.txt_buscar_nivel_id.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txt_buscar_nivel_id.BorderRadius = 10;
+            this.txt_buscar_nivel_id.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_buscar_nivel_id.DefaultText = "";
+            this.txt_buscar_nivel_id.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_buscar_nivel_id.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_buscar_nivel_id.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_buscar_nivel_id.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_buscar_nivel_id.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txt_buscar_nivel_id.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_buscar_nivel_id.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.txt_buscar_nivel_id.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.txt_buscar_nivel_id.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_buscar_nivel_id.Location = new System.Drawing.Point(556, 155);
+            this.txt_buscar_nivel_id.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_buscar_nivel_id.Name = "txt_buscar_nivel_id";
+            this.txt_buscar_nivel_id.PasswordChar = '\0';
+            this.txt_buscar_nivel_id.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(151)))), ((int)(((byte)(151)))));
+            this.txt_buscar_nivel_id.PlaceholderText = "Buscar por ID";
+            this.txt_buscar_nivel_id.SelectedText = "";
+            this.txt_buscar_nivel_id.Size = new System.Drawing.Size(135, 36);
+            this.txt_buscar_nivel_id.TabIndex = 43;
+            // 
+            // btn_adicionar
+            // 
+            this.btn_adicionar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn_adicionar.Animated = true;
+            this.btn_adicionar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_adicionar.BorderRadius = 10;
+            this.btn_adicionar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_adicionar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_adicionar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_adicionar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_adicionar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(79)))), ((int)(((byte)(82)))));
+            this.btn_adicionar.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.btn_adicionar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(210)))), ((int)(((byte)(197)))));
+            this.btn_adicionar.Image = ((System.Drawing.Image)(resources.GetObject("btn_adicionar.Image")));
+            this.btn_adicionar.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.btn_adicionar.ImageSize = new System.Drawing.Size(19, 19);
+            this.btn_adicionar.Location = new System.Drawing.Point(781, 152);
+            this.btn_adicionar.Name = "btn_adicionar";
+            this.btn_adicionar.ShadowDecoration.Depth = 5;
+            this.btn_adicionar.ShadowDecoration.Enabled = true;
+            this.btn_adicionar.Size = new System.Drawing.Size(144, 39);
+            this.btn_adicionar.TabIndex = 44;
+            this.btn_adicionar.Text = "Adicionar";
+            this.btn_adicionar.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
             // Tela_nivel_acesso
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
-            this.ClientSize = new System.Drawing.Size(1103, 622);
+            this.ClientSize = new System.Drawing.Size(1087, 583);
+            this.Controls.Add(this.btn_adicionar);
+            this.Controls.Add(this.txt_buscar_nivel_id);
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.txt_buscar_nivel);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.Lbl_estoque);
             this.Name = "Tela_nivel_acesso";
             this.Text = "Tela_nivel_acesso";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel Lbl_estoque;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2TextBox txt_buscar_nivel;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private Guna.UI2.WinForms.Guna2TextBox txt_buscar_nivel_id;
+        private Guna.UI2.WinForms.Guna2Button btn_adicionar;
     }
 }
